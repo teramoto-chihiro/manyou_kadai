a2c461991368c99d71283c25c03f4baab76187d7
teramoto-chihiro <komorikiri1031@gmail.com>
2021-05-20 05:06:43 +0000

    ビュー、ルーティング作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,15 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
