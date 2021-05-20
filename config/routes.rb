012ea6d30915acd8d34a4d5c271e05d01460b5e5
teramoto-chihiro <komorikiri1031@gmail.com>
2021-05-20 05:54:27 +0000

    タスクの新規作成、編集、削除追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tasks#index'
   resources :tasks
+  resources :tasks do
+    collection do
+      post :confirm
+    end
+  end
 end
