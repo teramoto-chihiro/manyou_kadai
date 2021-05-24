141c81ae7a42cdc9be661f117f33aa5f085184a1
teramoto-chihiro <komorikiri1031@gmail.com>
2021-05-24 01:53:56 +0000

    systemテスト、SQLバージョンup

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,7 +5,6 @@ describe 'タスクモデル機能', type: :model do
       it 'バリデーションにひっかる' do
         task = Task.new(title: '', content: '失敗テスト')
         expect(task).not_to be_valid
-        #not_to be_validはひっかかれば成功という意味
       end
     end
     context 'タスクの詳細が空の場合' do
@@ -16,9 +15,8 @@ describe 'タスクモデル機能', type: :model do
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(title: '成功テスト', content: '成功テスト1')
+        task = Task.new(title: '成功テスト', content: '成功テスト')
         expect(task).to be_valid
-        #to be_validはnotの逆、引っかからなければ成功という意味
       end
     end
   end
