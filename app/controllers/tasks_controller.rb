3107d520af6626c3cce4196ed9d2e4b0a8504c17
teramoto-chihiro <komorikiri1031@gmail.com>
2021-06-02 06:38:26 +0000

    優先順位実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,8 @@ class TasksController < ApplicationController
   def index
     if params[:sort_deadline]
       @tasks = Task.order(deadline: :desc)
+    elsif params[:sort_priority]
+    @tasks = Task.order(priority: :desc)
     elsif params[:search]
       if params[:search_title].present? && params[:search_status].present?
         @tasks = Task.search_title(params[:search_title]).search_status(params[:search_status])
@@ -56,6 +58,6 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   def task_params
-    params.require(:task).permit(:title, :content, :deadline, :status)
+    params.require(:task).permit(:title, :content, :deadline, :status, :priority)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_091504) do
+ActiveRecord::Schema.define(version: 2021_06_02_063434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_091504) do
     t.datetime "updated_at", null: false
     t.datetime "deadline", default: -> { "(now() + '1 mon'::interval)" }, null: false
     t.integer "status", default: 1, null: false
+    t.integer "priority", default: 1, null: false
     t.index ["title"], name: "index_tasks_on_title"
   end
 
