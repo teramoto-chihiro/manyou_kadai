82406ce804aadb7cae5d92a2fecaf97a4354a922
teramoto-chihiro <komorikiri1031@gmail.com>
2021-06-09 04:55:54 +0000

    シードデータ反映

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,18 @@ User.create!(name: name,
              admin: true
              )
 
+ 9.times do |n|
+   name = Faker::Games::Pokemon.name
+   email = Faker::Internet.email
+   password = "password"
+   User.create!(name: name,
+                email: email,
+                password: password,
+                password_confirmation: password,
+                admin: false
+                )
+ end
+
 Label.create!(
 [
   {name: 'HTML'},
@@ -29,3 +41,15 @@ Label.create!(
   {name: '卒業課題'}
 ]
 )
+
+10.times do |n|
+  title = Faker::Games::Pokemon.move
+  content = Faker::Games::Pokemon.location
+  expired_at = Faker::Date.between(from: '2021-05-27', to: '2021-09-27')
+  user_id = n + 1
+  Task.create!(title: title,
+               content: content,
+               deadline: deadline,
+               user_id: user_id,
+               )
+end
